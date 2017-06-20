ec5b3ba541b2eaaf123c35564607c241abc2b0d7
Kentaro Nakatsuka <kentaro.nakatsuka.sys@gmail.com>
2017-06-20 10:13:39 +0900

    first commit

---
+++ src/Shindan.java
@@ -0,0 +1,44 @@
+
+public class Shindan {
+
+	public static void main(String[] args) {
+		//メイン処理
+		int situ1 = situmon1();
+		//回答内容で診断結果を考える処理
+
+		//回答結果を出力
+
+
+	}
+
+	public static int situmon1() {
+		//質問内容を表示
+		//回答内容を表示(1：男性　2：女性)
+		//ユーザーが答えを入力
+		//返り値ありでもおｋ
+
+		int a = 0;
+		return a;
+
+	}
+
+	public void sirumon2() {
+
+	}
+
+	public void sirumon3() {
+
+	}
+
+	public void sirumon4() {
+
+	}
+
+	public void sirumon5() {
+
+	}
+
+	public void sirumon6() {
+
+	}
+}
